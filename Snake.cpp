a1b917107fb51a0a19964c8e44582c31c40dcdca
vominhduong-23520358 <23520358@gm.uit.edu.vn>
2025-11-16 21:20:39 +0700

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -56,3 +56,13 @@ int main()
 
     return 0;
 }
+void gotoxy( int column, int line )
+  {
+  COORD coord;
+  coord.X = column;
+  coord.Y = line;
+  SetConsoleCursorPosition(
+    GetStdHandle( STD_OUTPUT_HANDLE ),
+    coord
+    );
+  }
